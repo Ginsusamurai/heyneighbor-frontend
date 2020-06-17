d38ef34fba4da9ac8864b2540c4a3ccf9b55f8ce
Wenhao Piao <wenhaopiao520@gmail.com>
2020-06-17 14:38:22 -0700

    connect basic auth to backend

--- crystal-dashboard/src/pages/login/index.js
+++ crystal-dashboard/src/pages/login/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Login from './login.js';
+import { Route } from 'react-router-dom';
 
 
+// let LoginPage = props => (
+//   <div className='content'>
+//     <div className='container-fluid'>
+//       <Login  />
+//     </div>
+//   </div>
+// )
+
+// export default LoginPage;
 
 
-let LoginPage = props => (
-  <div className='content'>
-    <div className='container-fluid'>
-      <Login  />
+let Forms = ({ match }) => (
+  <div className="content">
+    <div className="container-fluid">
+      <Route path={`/loggedin`} render={props => {
+        return <Login {...props} onSubmit={values => alert(JSON.stringify(values, null, 2))} />
+      }} />
     </div>
   </div>
-)
+);
 
-export default LoginPage;
+export default Forms;
--- crystal-dashboard/src/pages/login/login.js
+++ crystal-dashboard/src/pages/login/login.js
@@ -1,10 +1,17 @@
 import React, { useEffect, component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import cookie from 'react-cookies';
 import { loginUser, saveMyUser, saveToken } from '../../reducers/user';
+import { Field, reduxForm } from 'redux-form';
+import renderField from 'components/FormInputs/renderField';
+require('dotenv').config();
 
-const Login = props => {
-  
+const superagent = require('superagent');
+const base64 = require('base-64');
+const BACKEND_ROOT = process.env.BACKEND_ROOT;
+
+let Login = props => {
+  const { submitting, submitForm, state } = props;
   console.log('login props', props, props.signup, cookie.load('token'));
   // if(props.signup === {} && cookie.load('token')){
   //   console.log('this is true');
@@ -12,13 +19,49 @@ const Login = props => {
   //   props.getUser(cookie.load('token'));
   // }
 
- 
-  useEffect( () => {props.saveTheToken(cookie.load('token'))},[]);
-  useEffect( () => {props.getUser(cookie.load('token'))},[]);
+  const handleSubmit = e => {
+    e.preventDefault();
+    const basic = base64.encode(`${e.target.userName.value}:${e.target.password.value}`);
+    superagent
+      .post(`${BACKEND_ROOT}/signin`)
+      .set('Authorization', `Basic ${basic}`)
+      .then(result => {
+        console.log('-----result------', result)
+      })
+  }
 
-  return(
+  useEffect(() => { props.saveTheToken(cookie.load('token')) }, []);
+  useEffect(() => { props.getUser(cookie.load('token')) }, []);
 
-    <span>hello</span>
+  return (
+    <div className="card">
+      <div className="header">
+        <h4>Log In</h4>
+      </div>
+      <div className="content">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label className="control-label">Username</label>
+            <Field
+              name="userName"
+              type="text"
+              required="true"
+              component={renderField} />
+          </div>
+
+          <div className="form-group">
+            <label className="control-label">password</label>
+            <Field
+              name="password"
+              type="password"
+              required="true"
+              component={renderField} />
+          </div>
+
+          <button type="submit" className="btn btn-fill btn-info" disabled={submitting}>Log in</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
@@ -29,8 +72,10 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = (dispatch) => ({
   saveTheToken: (token) => dispatch(saveToken(token)),
-  getUser:(token) => dispatch(loginUser(token)),
+  getUser: (token) => dispatch(loginUser(token)),
 })
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+Login = connect(mapStateToProps, mapDispatchToProps)(Login);
+
+export default reduxForm({ form: 'Login' })(Login);
